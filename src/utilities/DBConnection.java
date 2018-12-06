14d116d96db075c6b814e6005b2c0fd8dd0a2133
Abdullah Rehab <abdorehab95@gmail.com>
2018-12-06 09:47:38 +0200

    MVc

--- src/utilities/DBConnection.java
+++ src/utilities/DBConnection.java
@@ -1,4 +1,14 @@
 package utilities;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    static Connection connection;
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/bus_management", "root", "root");
+        return connection;
+    }
 }
